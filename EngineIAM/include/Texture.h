98f5e21fb4fdcad4b13e8cc507351ea1c29959eb
Ivancelot9 <134656635+Ivancelot9@users.noreply.github.com>
2024-06-04 10:39:36 -0600

    Implemetation Texture and SwapChain Class

---
+++ EngineIAM/include/Texture.h
@@ -0,0 +1,49 @@
+#pragma once
+#include "PreRequesites.h"
+
+class Device;
+class DeviceContext;
+
+class Texture
+{
+public:
+	Texture() = default;
+	~Texture() = default;
+	/// <summary>
+	/// 
+	/// </summary>
+	/// Brief: Crea una textura a partir de una imagen (.dds) desde nuestro ordenador
+	/// <param name="device"></param> Lllamamos al DEvice paea poder generar recuersos en la memoria
+	/// <param name="textureName"></param>Almacenar el nombre de la textura
+	HRESULT init(Device device, std::string textureName);
+	/// <summary>
+/// Brief: Este metodo es el reposable de crear Textura en 2D 
+/// </summary>
+	void init(Device device, unsigned int width, unsigned int height, 
+		      DXGI_FORMAT Format,
+		      unsigned int BindFlags);
+
+
+	void update();
+/// <summary>
+/// Brief: Este metodoco carga la actualizacion de la logica de la textura
+/// </summary>
+
+	void render(DeviceContext & deviceContext,
+		        unsigned int StartSlot, unsigned int NumViews);
+/// <summary>
+/// Brief: Este metodo se encarga de dibujar los recursos en la pantalla
+/// </summary>
+
+	/// <summary>
+	/// Este metodo se encarga de realizar los recursos almacenados en la memoria de la clase
+	/// </summary>
+	void destroy();
+
+
+public:
+	ID3D11Texture2D* m_texture = nullptr;//Esta variable maneja un recurso de textura como datos
+	ID3D11ShaderResourceView* m_textureFromImg;//Esta variable maneja un recurso de textura como datos de imagen
+
+};
+
--- EngineIAM/source/DeviceContext.cpp
+++ EngineIAM/source/DeviceContext.cpp
@@ -5,3 +5,19 @@ DeviceContext::destroy()
 {
 	SAFE_RELEASE(m_deviceContext);
 }
+
+void DeviceContext::PSSetShaderResources(unsigned int StartSlot, 
+	unsigned int NumViews, 
+	ID3D11ShaderResourceView* const* ppShaderResourceViews)
+{
+	if (ppShaderResourceViews == nullptr)
+	{
+		ERROR("DeviceContext", "PSSetShaderResources", "Check for ID3D11ShaderResourceView* const* ppShaderResourceViews")
+			exit(1);
+	}
+	else
+	{
+		m_deviceContext->PSSetShaderResources(StartSlot, NumViews, ppShaderResourceViews);
+
+	}
+}
