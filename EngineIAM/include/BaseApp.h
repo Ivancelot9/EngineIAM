3e0a2211088d86abeda25181412cb1dce590dd02
Ivancelot9 <134656635+Ivancelot9@users.noreply.github.com>
2024-08-07 19:13:04 -0600

    Base App Implementation Complete

--- EngineIAM/include/BaseApp.h
+++ EngineIAM/include/BaseApp.h
@@ -1,6 +1,20 @@
 #pragma once
-#include "PreRequesites.h"
-#include "Window.h"
+#include "PreRequesites.h"  // Archivo de encabezado que contiene pre-requisitos
+#include "Window.h"         // Archivo de encabezado para la ventana
+#include "Device.h"         // Archivo de encabezado para el dispositivo
+#include "DeviceContext.h"  // Archivo de encabezado para el contexto del dispositivo
+#include "SwapChain.h"      // Archivo de encabezado para la cadena de intercambio
+#include "Texture.h"        // Archivo de encabezado para las texturas
+#include "DepthStencilView.h" // Archivo de encabezado para la vista de stencil de profundidad
+#include "RenderTargetView.h" // Archivo de encabezado para la vista de destino de renderizado
+#include "Viewport.h"       // Archivo de encabezado para el viewport
+#include "ShaderProgram.h"  // Archivo de encabezado para el programa de shaders
+#include "Buffer.h"         // Archivo de encabezado para el buffer
+#include "SamplerState.h"   // Archivo de encabezado para el estado del sampler
+#include "ModelLoader.h"    // Archivo de encabezado para cargar modelos
+#include "UserInterface.h" //Archivo de cabeza
+#include "Actor.h"
+
 class BaseApp
 {
 public:
@@ -15,12 +29,47 @@ public:
 
 	void destroy();
 
-	int run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow);
+	int run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow, WNDPROC wndproc);
 
-	LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
-
+	void CreateGrid(int width, int depth, float spacing);
 private:
 
-	Window m_window;
+	Window                              m_window;                    // Ventana
+	Device                              m_device;                    // Dispositivo
+	DeviceContext                       m_deviceContext;             // Contexto del dispositivo
+	SwapChain                           m_swapchain;                 // Cadena de intercambio
+	Texture                             m_backBuffer;                // Textura de buffer trasero
+	Texture                             m_depthStencil;              // Textura de stencil de profundidad
+	DepthStencilView                    m_depthStencilView;          // Vista de stencil de profundidad
+	RenderTargetView                    m_renderTargetView;          // Vista de destino de renderizado
+	Viewport                            m_viewport;                  // Viewport
+	ShaderProgram                       m_ShaderProgram;             // Programa de shaders
+
+	std::vector<Buffer>                 m_vertexBuffers;             // Vectores de buffers de vértices
+	std::vector<Buffer>                 m_indexBuffers;              // Vectores de buffers de índices
+
+	Buffer                              m_CBBufferNeverChanges;      // Buffer constante que nunca cambia
+	Buffer                              m_CBBufferChangeOnResize;    // Buffer constante que cambia al redimensionar
+	Buffer                              m_CBBufferChangesEveryFrame; // Buffer constante que cambia cada cuadro
+
+	std::vector<Texture>                modelTextures;               // Texturas del modelo
+	UserInterface                       m_UserInterface;
+	SamplerState                        m_sampler;                   // Estado del sampler
+	ModelLoader                         m_model;                     // Cargador de modelos
+	Texture                             m_default;                   // Textura predeterminada
+	XMMATRIX                            m_View;                      // Matriz de vista
+	XMMATRIX                            m_Projection;                // Matriz de proyección
+	XMFLOAT4                            m_vMeshColor;              // Color del modelo
+	CBNeverChanges                      cbNeverChanges;              // Estructura de constantes que nunca cambian
+	CBChangeOnResize                    cbChangesOnResize;           // Estructura de constantes que cambian al redimensionar
+	CBChangesEveryFrame                 cb;                          // Estructura de constantes que cambian cada cuadro
+
+
+	//Vela Actor
+	EngineUtilities::TSharedPointer<Actor> AVela;
+	//Grid Actor
+	MeshComponent                          MC;
+	EngineUtilities::TSharedPointer<Actor> AGrid;
+	std::vector<Texture> gridTexs;
 
 };
--- EngineIAM/source/BaseApp.cpp
+++ EngineIAM/source/BaseApp.cpp
@@ -2,27 +2,219 @@
 
 HRESULT BaseApp::init()
 {
-    return E_NOTIMPL;
+    HRESULT hr = S_OK;
+
+    //Create SwapChain
+    m_swapchain.init(m_device, m_deviceContext, m_backBuffer, m_window);
+    // Create a render target view
+    m_renderTargetView.init(m_device, m_backBuffer, DXGI_FORMAT_R8G8B8A8_UNORM);
+    // Create depth stencil texture
+    m_depthStencil.init(m_device, m_window.m_width,
+        m_window.m_height,
+        DXGI_FORMAT_D24_UNORM_S8_UINT,
+        D3D11_BIND_DEPTH_STENCIL);
+    //Create the depth stencil view
+    m_depthStencilView.init(m_device, m_depthStencil, DXGI_FORMAT_D24_UNORM_S8_UINT);
+    // Setup the viewport
+    m_viewport.init(m_window);
+
+
+    // Define the input layout
+    std::vector<D3D11_INPUT_ELEMENT_DESC> Layout;
+
+    D3D11_INPUT_ELEMENT_DESC position;
+    position.SemanticName = "POSITION";
+    position.SemanticIndex = 0;
+    position.Format = DXGI_FORMAT_R32G32B32_FLOAT;
+    position.InputSlot = 0;
+    position.AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT /*12*/;
+    position.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+    position.InstanceDataStepRate = 0;
+    Layout.push_back(position);
+
+    D3D11_INPUT_ELEMENT_DESC texcoord;
+    texcoord.SemanticName = "TEXCOORD";
+    texcoord.SemanticIndex = 0;
+    texcoord.Format = DXGI_FORMAT_R32G32_FLOAT;
+    texcoord.InputSlot = 0;
+    texcoord.AlignedByteOffset = D3D11_APPEND_ALIGNED_ELEMENT /*12*/;
+    texcoord.InputSlotClass = D3D11_INPUT_PER_VERTEX_DATA;
+    texcoord.InstanceDataStepRate = 0;
+    Layout.push_back(texcoord);
+
+    m_ShaderProgram.init(m_device, "EngineIAM.fx", Layout);
+
+    //Create Grid
+    CreateGrid(50, 50, 25.0f);
+
+    m_model.LoadModel("Models/Vela2.fbx");
+
+
+    // Inicialización de Constant Buffers
+    m_CBBufferNeverChanges.init(m_device, sizeof(CBNeverChanges));
+
+    m_CBBufferChangeOnResize.init(m_device, sizeof(CBChangeOnResize));
+
+    Texture Vela_Char_BaseColor;
+    Vela_Char_BaseColor.init(m_device, "Textures/Vela/Vela_Char_BaseColor.png", ExtensionType::PNG);
+
+    Texture Vela_Corneas_BaseColor;
+    Vela_Corneas_BaseColor.init(m_device, "Textures/Vela/Vela_Corneas_BaseColor.png", ExtensionType::PNG);
+
+    Texture Vela_Gun_BaseColor;
+    Vela_Gun_BaseColor.init(m_device, "Textures/Vela/Vela_Gun_BaseColor.png", ExtensionType::PNG);
+
+    Texture Vela_Legs_BaseColor;
+    Vela_Legs_BaseColor.init(m_device, "Textures/Vela/Vela_Legs_BaseColor.png", ExtensionType::PNG);
+
+    Texture Vela_Mechanical_BaseColor;
+    Vela_Mechanical_BaseColor.init(m_device, "Textures/Vela/Vela_Mechanical_BaseColor.png", ExtensionType::PNG);
+
+    Texture Vela_Plate_BaseColor;
+    Vela_Plate_BaseColor.init(m_device, "Textures/Vela/Vela_Plate_BaseColor.png", ExtensionType::PNG);
+
+    Texture Vela_Visor_BaseColor;
+    Vela_Visor_BaseColor.init(m_device, "Textures/Vela/Vela_Visor_BaseColor.png", ExtensionType::PNG);
+
+    modelTextures.push_back(Vela_Corneas_BaseColor);
+    modelTextures.push_back(Vela_Gun_BaseColor);
+    modelTextures.push_back(Vela_Visor_BaseColor);
+    modelTextures.push_back(Vela_Legs_BaseColor);
+    modelTextures.push_back(Vela_Mechanical_BaseColor);
+    modelTextures.push_back(Vela_Char_BaseColor);
+    modelTextures.push_back(Vela_Plate_BaseColor);
+
+    m_default.init(m_device, "Textures/Default.png", ExtensionType::PNG);
+
+    // Initialize the view matrix
+    XMVECTOR Eye = XMVectorSet(0.0f, 3.0f, -6.0f, 0.0f);
+    XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+    XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
+    m_View = XMMatrixLookAtLH(Eye, At, Up);
+
+    cbNeverChanges.mView = XMMatrixTranspose(m_View);
+
+
+    // Initialize the projection matrix
+    m_Projection = XMMatrixPerspectiveFovLH(XM_PIDIV4, m_window.m_width / (FLOAT)m_window.m_height, 0.01f, 100.0f);
+
+    cbChangesOnResize.mProjection = XMMatrixTranspose(m_Projection);
+
+    // Set Vela Actor
+    AVela = EngineUtilities::MakeShared<Actor>(m_device);
+
+    if (!AVela.isNull()) {
+        MESSAGE("Actor", "Actor", "Actor accessed successfully.")
+
+            AVela->getComponent<Transform>()->setPosition(EngineUtilities::Vector3(-0.9f, -2.0f, 2.0f));
+        AVela->getComponent<Transform>()->setRotation(EngineUtilities::Vector3(XM_PI / -2.0f, 0.0f, XM_PI / 2.0f));
+        AVela->getComponent<Transform>()->setScale(EngineUtilities::Vector3(.03f, .03f, .03f));
+        AVela->setMesh(m_device, m_model.meshes);
+        AVela->setTextures(modelTextures);
+    }
+    else {
+        MESSAGE("Actor", "Actor", "Actor resource not found.")
+    }
+
+    AGrid = EngineUtilities::MakeShared<Actor>(m_device);
+    if (!AGrid.isNull()) {
+        std::vector<MeshComponent> gridMesh;
+        gridMesh.push_back(MC);
+        AGrid->setMesh(m_device, gridMesh);
+        gridTexs.push_back(m_default);
+        AGrid->setTextures(gridTexs);
+        AGrid->getComponent<Transform>()->setPosition(EngineUtilities::Vector3(0.0f,
+            -2.0f,
+            0.0f));
+        AGrid->getComponent<Transform>()->setScale(EngineUtilities::Vector3(.03f,
+            .03f,
+            .03f));
+        MESSAGE("Actor", "Actor", "Actor accessed successfully.")
+    }
+    else {
+        MESSAGE("Actor", "Actor", "Actor resource not found.")
+    }
+
+    m_UserInterface.init(m_window.m_hWnd, m_device.m_device, m_deviceContext.m_deviceContext);
+    return S_OK;
 }
 
 void BaseApp::update()
 {
+
+    m_UserInterface.update();
+    bool show_demo_window = true;
+
+    ImGui::ShowDemoWindow(&show_demo_window);
+   
+    m_CBBufferNeverChanges.update(m_deviceContext, 0, nullptr, &cbNeverChanges, 0, 0);
+    m_CBBufferChangeOnResize.update(m_deviceContext, 0, nullptr, &cbChangesOnResize, 0, 0);
+
+    // Actualizar info logica del mesh
+    AVela->getComponent<Transform>()->ui_noWindow("Transform");
+    AVela->update(0, m_deviceContext);
+    AGrid->update(0, m_deviceContext);
+    
 }
 
 void BaseApp::render()
 {
+    // Clear the back buffer
+    float ClearColor[4] = { 0.85f, 0.85f, 0.85f, 1.0f }; // red, green, blue, alpha
+    m_renderTargetView.render(m_deviceContext, m_depthStencilView, 1, ClearColor);
+
+    // Set Viewport
+    m_viewport.render(m_deviceContext);
+    // Clear the depth buffer to 1.0 (max depth)
+    m_depthStencilView.render(m_deviceContext);
+
+    // Render the cube
+    m_ShaderProgram.render(m_deviceContext);
+
+    // Render the models
+    AVela->render(m_deviceContext);
+    AGrid->render(m_deviceContext);
+
+
+    // Actualizar constant buffers
+    m_CBBufferNeverChanges.render(m_deviceContext, 0, 1);
+    m_CBBufferChangeOnResize.render(m_deviceContext, 1, 1);
+
+    m_UserInterface.render();
+
+    m_swapchain.present();
 }
 
 void BaseApp::destroy()
 {
+    if (m_deviceContext.m_deviceContext) m_deviceContext.m_deviceContext->ClearState();
+
+    AVela->destroy();
+    AGrid->destroy();
+
+    //m_default.destroy();
+    m_CBBufferNeverChanges.destroy();
+    m_CBBufferChangeOnResize.destroy();
+
+    m_ShaderProgram.destroy();
+    m_depthStencil.destroy();
+    m_depthStencilView.destroy();
+    m_renderTargetView.destroy();
+    m_swapchain.destroy();
+    m_deviceContext.destroy();
+    // Release UI
+    m_UserInterface.destroy();
+
+    m_device.destroy();
+
 }
 
-int BaseApp::run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+int BaseApp::run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow, WNDPROC wndproc)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
-    if (FAILED(m_window.init(hInstance, nCmdShow, nullptr)))
+    if (FAILED(m_window.init(hInstance, nCmdShow, wndproc)))
         return 0;
 
     if (FAILED(init()))
@@ -52,26 +244,57 @@ int BaseApp::run(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine,
     return (int)msg.wParam;
 }
 
-LRESULT CALLBACK BaseApp::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+void BaseApp::CreateGrid(int width, int depth, float spacing)
 {
-    PAINTSTRUCT ps;
-    HDC hdc;
-
-    switch (message)
+    MC.m_vertex.clear();
+    MC.m_index.clear();
+    float halfLineWidth = 0.8 * 0.5f;
+    for (int i = -width; i <= width; ++i)
     {
-    case WM_PAINT:
-        hdc = BeginPaint(hWnd, &ps);
-        EndPaint(hWnd, &ps);
-        break;
+        //Create vertices for a vertical line as two triangules
+        MC.m_vertex.push_back({ XMFLOAT3(i * spacing - halfLineWidth, 0, -depth * spacing),
+                                XMFLOAT2(0.0f, 0.0f) });
+        MC.m_vertex.push_back({ XMFLOAT3(i * spacing + halfLineWidth, 0, -depth * spacing),
+                                XMFLOAT2(0.0f, 0.0f) });
+        MC.m_vertex.push_back({ XMFLOAT3(i * spacing - halfLineWidth, 0, depth * spacing),
+                                XMFLOAT2(0.0f, 0.0f) });
+        MC.m_vertex.push_back({ XMFLOAT3(i * spacing + halfLineWidth, 0, depth * spacing),
+                                XMFLOAT2(0.0f, 0.0f) });
 
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
+        MC.m_index.push_back(MC.m_vertex.size() - 4);
+        MC.m_index.push_back(MC.m_vertex.size() - 3);
+        MC.m_index.push_back(MC.m_vertex.size() - 2);
 
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+        MC.m_index.push_back(MC.m_vertex.size() - 3);
+        MC.m_index.push_back(MC.m_vertex.size() - 2);
+        MC.m_index.push_back(MC.m_vertex.size() - 1);
     }
 
-    return 0;
+    for (int i = -depth; i <= depth; ++i)
+    {
+        //Create vertices for a horizontal line as two triangules
+        MC.m_vertex.push_back({ XMFLOAT3(-width * spacing,0, i * spacing - halfLineWidth),
+                               XMFLOAT2(0.0f, 0.0f) });
+        MC.m_vertex.push_back({ XMFLOAT3(width * spacing,0, i * spacing - halfLineWidth),
+                              XMFLOAT2(0.0f, 0.0f) });
+        MC.m_vertex.push_back({ XMFLOAT3(-width * spacing,0, i * spacing + halfLineWidth),
+                              XMFLOAT2(0.0f, 0.0f) });
+        MC.m_vertex.push_back({ XMFLOAT3(width * spacing,0, i * spacing + halfLineWidth),
+                              XMFLOAT2(0.0f, 0.0f) });
+
+
+        MC.m_index.push_back(MC.m_vertex.size() - 4);
+        MC.m_index.push_back(MC.m_vertex.size() - 3);
+        MC.m_index.push_back(MC.m_vertex.size() - 2);
+
+        MC.m_index.push_back(MC.m_vertex.size() - 3);
+        MC.m_index.push_back(MC.m_vertex.size() - 2);
+        MC.m_index.push_back(MC.m_vertex.size() - 1);
+    }
+
+    MC.m_numVertex = MC.m_vertex.size();
+    MC.m_numIndex = MC.m_index.size();
 }
+
+
 
